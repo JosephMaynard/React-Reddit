afe2c11d68f780b61b0879007e952decc6d7328e
Joseph Maynard <joseph.maynard@gmail.com>
2016-09-10 10:24:48 +1000

    Add loadMorePosts function to PostList component

--- src/PostList.js
+++ src/PostList.js
@@ -9,6 +9,8 @@ class PostList extends Component {
 
     this.state = {
       posts: [],
+      loadedPosts: [],
+      postNumber: 25,
     };
   }
 
@@ -16,8 +18,8 @@ class PostList extends Component {
     const _this = this;
     this.serverRequest =
       axios
-        .get(`https://www.reddit.com/r/${ this.props.subreddits.join('+') }/.json`)
-        .then(function(result) {    
+        .get(`https://www.reddit.com/r/${ this.props.subreddits.join('+') }/.json?raw_json=1`)
+        .then(result => {   
           console.log(result);
           _this.setState({
             posts: result.data.data.children,
@@ -25,10 +27,38 @@ class PostList extends Component {
         });
   }
 
+  loadMorePosts() {
+    const _this = this;
+    this.serverRequest =
+      axios
+        .get(`https://www.reddit.com/r/${ this.props.subreddits.join('+') }/.json?count=${
+              this.state.postNumber + 25
+            }&after=${
+              this.state.loadedPosts[this.state.loadedPosts.length - 1]}&raw_json=1`)
+        .then(result => {
+          console.log(result);
+          const newPosts = [];
+          const newPostsLoaded = [];
+          for(const post of result.data.data.children){
+            if(this.state.loadedPosts.indexOf(post.data.name) === -1){
+              newPosts.push(post);
+              newPostsLoaded.push(post.data.name);
+            }
+          }
+          _this.setState({
+            posts: _this.state.posts.concat(newPosts),
+            loadedPosts: _this.state.loadedPosts.concat(newPostsLoaded),
+            postNumber: _this.state.postNumber + 25,
+          });
+        });
+  }
+
   render() {
     return (
       <div>
-        {this.state.posts.map(result => <Post key={result.data.id} data={result.data} />)}
+        {this.state.posts.map(result => <Post key={result.data.name + (Math.round(Math.random() * 1e8)).toString(36)} data={result.data} />)
+        }
+        <button onClick={this.loadMorePosts.bind(this)}>Load More</button>
       </div>
     );
   }
