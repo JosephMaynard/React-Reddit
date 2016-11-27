98885cc312eecd12a40c5a69d024536c5d07df44
Joseph Maynard <joseph.maynard@gmail.com>
2016-11-27 21:41:10 +1100

    Add react-headroom to hide/show Header on scroll

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import Headroom from 'react-headroom';
 import logo from './logo.svg';
 import MenuBtn from './MenuBtn.svg';
 import './Header.css';
 
 const Header = (props) => (
-  <div className="Header">
-    <div className="inner">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React Reddit</h2>
-      <img src={MenuBtn} className="MenuBtn" alt="Menu" onClick={props.toggleMenu} />
+  <Headroom>
+    <div className="Header">
+      <div className="inner">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Welcome to React Reddit</h2>
+        <img src={MenuBtn} className="MenuBtn" alt="Menu" onClick={props.toggleMenu} />
+      </div>
     </div>
-  </div>
+  </Headroom>
 );
 
 Header.propTypes = {
